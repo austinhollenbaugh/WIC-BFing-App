98aa0a96f8077092f1de4e45137fd2badd69a608
Austin <austinhollenbaugh@gmail.com>
2016-08-27 21:40:23 -0600

    waiting modal working

--- public/app/component/chat/clientChat/clientChatLandingCtrl.js
+++ public/app/component/chat/clientChat/clientChatLandingCtrl.js
@@ -1,25 +1,22 @@
 angular.module('bfing-app')
   .controller('clientChatLandingCtrl', function($scope, mainService) {
 
-    $scope.userQ = [];
+    var showModal = false;
 
     $scope.addUserToQ = function(clientID) {
       console.log('user added');
+      showModal = true;
       $scope.$emit('addUserToQ', clientID);
 
     };
 
-    $scope.userJoinChat =
-
     $scope.$on('userAdded', function(ev, clientID) {
-      $scope.userQ.push(clientID);
+      // $scope.userQ.push(clientID);
     });
 
     mainService.getUser().then(function(response) {
       $scope.user = response.data.displayName;
-
       $scope.id = response.data.id;
-
       console.log(response.data);
 
       if (response.data.err) {
